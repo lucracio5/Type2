6b1a116ac49feb6e6b4294175f356ce5c83fe661
lucracio5 <lucracio50@gmail.com>
2025-06-10 18:08:29 -0500

    Revert "Merge branch 'New-Main' of https://github.com/lucracio5/Type2 into New-Main"
    This reverts commit c42ad36b9424deb8f00007049f054c3b4f5f397d, reversing
    changes made to d3aeb323c73ea03403f14802f23c0631a1dca80b.

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -5,59 +5,39 @@ using UnityEngine;
 public class CameraMover : MonoBehaviour
 {
 
-    [SerializeField] private Vector3 defaultPosition = new Vector3(150, 35, 0);
-    [SerializeField] private Vector3 defaultRotation = new Vector3(20, 180, 0);
-    [SerializeField] private float defaultFOV = 60f;
-
     [SerializeField] private float xMin = 10f;
     [SerializeField] private float xMax = 230f;
-
     [SerializeField] private float zMin = -200f;
     [SerializeField] private float zMax = 30f;
-
-    [SerializeField] private float movementSpeed = 10f;
-    [SerializeField] private float zoomSpeed = 15f;
-    [SerializeField] private float rotationSpeed = 5f;
-
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float zoomSpeed = 10f;
     [SerializeField] private float minFOV = 12f;
     [SerializeField] private float maxFOV = 92f;
-
     private float lastMousePosX;
     private float lastMousePosY;
 
-    private float curDeltaX;
-    private float curDeltaY;
+
 
     // Update is called once per frame
     void Update()
     {
-        TrackMouse();
-
         MoveCamera();
         ZoomCamera();
-        RotateCamera();
-    }
-
-
-    void TrackMouse() {
-        Vector3 mousePos = Input.mousePosition; // Get the current mouse position in screen coordinates
-
-        //Gets the mouse position relative to the last frame
-        curDeltaX = mousePos.x - lastMousePosX;
-        curDeltaY = mousePos.y - lastMousePosY;
-
-        //updates the last mouse position
-        lastMousePosX = mousePos.x;
-        lastMousePosY = mousePos.y;
     }
 
     // Moves the camera based on mouse input
     void MoveCamera()
     {
+        Vector3 mousePos = Input.mousePosition; // Get the current mouse position in screen coordinates
+
+        //Gets the mouse position relative to the last frame
+        float deltaX = mousePos.x - lastMousePosX;
+        float deltaY = mousePos.y - lastMousePosY;
+
         bool cameraIsInBounds = transform.position.x > xMin && transform.position.x < xMax && transform.position.z > zMin && transform.position.z < zMax;
         if (cameraIsInBounds && Input.GetMouseButton(1)) // If the right mouse button is pressed and the camera is within bounds, move the camera
         {
-            transform.position = transform.position + new Vector3(curDeltaX * movementSpeed * Time.deltaTime, 0, curDeltaY * movementSpeed * Time.deltaTime);
+            transform.position = transform.position + new Vector3(deltaX * speed * Time.deltaTime, 0, deltaY * speed * Time.deltaTime);
         }
         else if (!cameraIsInBounds) // If the camera is out of bounds, snap the camera position to within the bounds
         {
@@ -68,6 +48,9 @@ public class CameraMover : MonoBehaviour
             );
         }
 
+        //updates the last mouse position
+        lastMousePosX = mousePos.x;
+        lastMousePosY = mousePos.y;
     }
 
 
@@ -84,20 +67,7 @@ public class CameraMover : MonoBehaviour
         {
             Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView - scrollDelta.y * zoomSpeed, minFOV, maxFOV); // Clamp the field of view to the specified limits
         }
-    }
 
-    void RotateCamera() {
-        if (Input.GetMouseButton(2)) // If the scroll wheel button is pressed, rotate the camera
-        {
-            transform.Rotate(-1f * curDeltaY * rotationSpeed * Time.deltaTime, curDeltaX * rotationSpeed * Time.deltaTime, 0); //rotate the camera about the x and y axis
-            //Debug.Log("curDeltaX: " + curDeltaX + ", curDeltaY: " + curDeltaY);
-        }
-    }
 
-    //resets the camera to its default state
-    public void ResetCamera() {
-        transform.position = defaultPosition;
-        Camera.main.fieldOfView = defaultFOV;
-        transform.Rotate(defaultRotation);
     }
 }
