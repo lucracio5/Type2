264dbef070ee671cab026df6dae798e2e9edeb8e
lucracio5 <lucracio50@gmail.com>
2025-07-27 22:21:23 -0500

    Start of AI

--- Assets/Scripts/Nav_test.cs
+++ Assets/Scripts/Nav_test.cs
@@ -9,15 +9,24 @@ public class Nav_test : MonoBehaviour
     public List<GameObject> targets;
     public int active_target = 0;
     public float min_distance = 5f;
+    public Variable_Tracker tracker;
 
     private List<int> passed = new List<int>();
     private bool justArrived = false;
     private int roverID;
+    public int speed;
+    public int baterey_life;
+    public int mining_spped;
+    public bool on_trip;
 
     void Start()
     {
+        tracker = GameObject.Find("Game Manager").GetComponent<Variable_Tracker>();
+        int RoverId = GetRoverID();
+        speed = tracker.roverSlotStats[RoverId][0];
+        baterey_life = tracker.roverSlotStats[RoverId][1];
+        mining_spped = tracker.roverSlotStats[RoverId][2];
         active_target = 0;
-        agent.destination = targets.Count > 0 ? targets[active_target].transform.position : home.transform.position;
 
     }
 
@@ -27,6 +36,29 @@ public class Nav_test : MonoBehaviour
         string roverName = this.gameObject.name;
         string number = System.Text.RegularExpressions.Regex.Match(roverName, @"\d+$").Value;
         return int.Parse(number);
+    }
+    public void start_trip()
+    {
+        on_trip = true;
+        int num = Random.Range(0, 3);//Random to decide which direction
+        if (num  == 0)
+        {
+
+        }
+        else if(num == 1)
+        {
+
+        }
+        else if (num == 2)
+        {
+
+        }
+        else if (num == 3)
+        {
+
+        }
+        agent.destination = targets[active_target].transform.position;
+
     }
 
     void Update()
